bf60a50659e5531b988aecda7bb0e192b088ab64
Priyanka Goyal <priyankagoyal2093@gmail.com>
2018-02-26 12:52:23 -0600

    added config reading

--- src/main/java/Node/Application.java
+++ src/main/java/Node/Application.java
@@ -4,16 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.messaging.simp.stomp.StompSession;
+
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
 public class Application {
     public static void main(String[] args) {
-        log.trace("--------before running application");
+
         ApplicationContext context = SpringApplication.run(Application.class, args);
-        StartLeaderElectionAndBfsTree startLeaderElectionAndBfsTree = (StartLeaderElectionAndBfsTree)context.getBean(StartLeaderElectionAndBfsTree.class);
-        Thread thread = new Thread(startLeaderElectionAndBfsTree);
-        log.trace("--------before running sendLeaderElection thread");
+        WebSocketConnector webSocketConnector = context.getBean(WebSocketConnector.class);
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                log.info("thread interrupted!");
+            }
+            List<StompSession> sessions = webSocketConnector.getSessions();
+            RootController rootController = context.getBean(RootController.class);
+            rootController.sendLeaderElection();
+        });
         thread.start();
     }
 }
